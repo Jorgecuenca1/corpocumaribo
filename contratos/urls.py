b2e006dfe239eee233c7900b8500e182885b2ae3
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2024-05-24 21:50:41 -0500

    Kk

--- contratos/urls.py
+++ contratos/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import lista_contratos, detalle_contrato
+from .views import lista_contratos, detalle_contrato,index
 
 urlpatterns = [
-    path('', lista_contratos, name='contratos'),
+    path('', index, name='index'),
+    path('contratos', lista_contratos, name='contratos'),
     path('contrato/<int:id>/', detalle_contrato, name='detalle_contrato'),
 ]
--- contratos/views.py
+++ contratos/views.py
@@ -8,3 +8,6 @@ def lista_contratos(request):
 def detalle_contrato(request, id):
     contrato = get_object_or_404(Contrato, id=id)
     return render(request, 'detalle_contrato.html', {'contrato': contrato})
+
+def index(request):
+    return render(request, 'index.html')
