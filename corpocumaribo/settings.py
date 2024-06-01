40979e3c847594dd76341e706b9a4a4767c28342
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2024-05-31 23:53:03 -0500

    kk

--- corpocumaribo/settings.py
+++ corpocumaribo/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = "django-insecure-p3=i*=-(6m8&r$u3x%wc$czeomo(4tj6%05b_fv-tq&t235t&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','corpocumaribo.co','demo.corpocumaribo.co','https://demo.corpocumaribo.co']
+ALLOWED_HOSTS = ['*','corpocumaribo.co','demo.corpocumaribo.co','https://corpocumaribo.co','https://www.corpocumaribo.co','https://demo.corpocumaribo.co']
 CSRF_TRUSTED_ORIGINS = [
     'https://corpocumaribo.co',
     'https://demo.corpocumaribo.co',
 ]
-CORS_ORIGIN_WHITELIST = ['corpocumaribo.co','demo.corpocumaribo.co','https://demo.corpocumaribo.co']
+CORS_ORIGIN_WHITELIST = ['corpocumaribo.co','demo.corpocumaribo.co','https://corpocumaribo.co','https://www.corpocumaribo.co','https://demo.corpocumaribo.co']
 # Application definition
 
 INSTALLED_APPS = [
