d8f694f3667627de8dfe42a6b18526360766b8e0
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2024-11-07 13:42:17 -0500

    kk

--- contratos/models.py
+++ contratos/models.py
@@ -1,31 +1,26 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-from django.db import models
-
 class Contrato(models.Model):
-    ANO = models.IntegerField()
-    contrato_no = models.CharField(max_length=100)
-    tipo_de_contrato = models.CharField(max_length=100)
-    nombre = models.CharField(max_length=255)
-    entidad = models.CharField(max_length=255)
+    ANO = models.CharField(max_length=4, blank=True, null=True)  # cambiado a CharField
+    contrato_no = models.CharField(max_length=100, blank=True, null=True)
+    tipo_de_contrato = models.CharField(max_length=100, blank=True, null=True)
+    nombre = models.CharField(max_length=255, blank=True, null=True)
+    entidad = models.CharField(max_length=255, blank=True, null=True)
     rup = models.CharField(max_length=100, blank=True, null=True)
-    contrato = models.CharField(max_length=100)
-    smmlv = models.DecimalField(max_digits=10, decimal_places=2)
+    contrato = models.CharField(max_length=100, blank=True, null=True)
+    smmlv = models.CharField(max_length=20, blank=True, null=True)
     secop = models.CharField(max_length=100, blank=True, null=True)
     contrato2 = models.CharField(max_length=100, blank=True, null=True)
     acta_final = models.CharField(max_length=100, blank=True, null=True)
-    objeto = models.TextField()
+    objeto = models.TextField(blank=True, null=True)
     fecha_inicio = models.CharField(max_length=100, blank=True, null=True)
     fecha_final = models.CharField(max_length=100, blank=True, null=True)
-    valor = models.DecimalField(max_digits=15, decimal_places=2)
-    smmlv3 = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    adicion = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
-    part_porc = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Part. %")
-    valor_total = models.DecimalField(max_digits=15, decimal_places=2)
-    smmlv4 = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    valor = models.CharField(max_length=20, blank=True, null=True)
+    smmlv3 = models.CharField(max_length=20, blank=True, null=True)
+    adicion = models.CharField(max_length=20, blank=True, null=True)
+    part_porc = models.CharField(max_length=10, blank=True, null=True, verbose_name="Part. %")
+    valor_total = models.CharField(max_length=20, blank=True, null=True)
+    smmlv4 = models.CharField(max_length=20, blank=True, null=True)
     is_visible = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
